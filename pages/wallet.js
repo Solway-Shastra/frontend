9df3e98a7b1c77faa936da0b14aeb0bced1d689d
Vivek Pal <vivek@shastraos.com>
2023-01-27 20:02:13 +0000

    wallet.js

---
+++ pages/wallet.js
@@ -0,0 +1,37 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect } from 'react'
+
+
+const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    if (window?.solana?.isPhantom) {
+      console.log('Phantom wallet found!');
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">Connect Your Wallet</p>
+          <p className="sub-text">
+            Get Started by connecting your wallet.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
