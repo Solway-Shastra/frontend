df660f9176adb59b756f516663240069b063955b
Vivek Pal <vivek@shastraos.com>
2023-01-28 23:19:34 +0000

    solana wallets

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,18 @@ import "../styles/globals.css";
 import Head from "next/head";
 import NProgress from "nprogress";
 import Router from "next/router";
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
 
 NProgress.configure({ showSpinner: false });
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
+require('@solana/wallet-adapter-react-ui/styles.css');
+require('../styles/globals.css');
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -33,6 +40,28 @@ function MyApp({ Component, pageProps }) {
 
 export default MyApp;
 
-{
-  /* deeyes36acfed */
-}
+const WalletApp = (props) => {
+  const { Component, pageProps } = props;
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+      () => [
+
+          new UnsafeBurnerWalletAdapter(),
+      ],
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [network]
+  );
+
+  return (
+      <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+              <WalletModalProvider>
+                  <Component {...pageProps} />
+              </WalletModalProvider>
+          </WalletProvider>
+      </ConnectionProvider>
+  );
+};
