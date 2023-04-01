0132efd5891d2c0f9c65d6a849ea2c3b99a393e8
Vivek <vivek@shastraos.org>
2023-04-01 19:19:38 +0530

    new branch

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['links.papareact.com'],
+  env: {
+    REACT_APP_CLUSTER: process.env.REACT_APP_CLUSTER,
   },
-};
+  images: {
+    domains: [
+      'a0.muscache.com'
+    ],
+  },
+}
 
+module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,67 +1,22 @@
-/* eslint-disable @next/next/no-page-custom-font */
-import "nprogress/nprogress.css";
-import "../styles/globals.css";
-import Head from "next/head";
-import NProgress from "nprogress";
-import Router from "next/router";
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import '../styles/globals.css'
 
-NProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
+const WalletConnectionProvider = dynamic(() => import('../context/WalletConnectionProvider'), {
+    ssr: false,
+})
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-        <title>Solway - Book Your Trips</title>
-        <meta
-          name="description"
-          content="Find holiday rentals, cabins, beach houses, unique homes and experiences around the world – all made possible by Hosts on Solway."
-        />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-
-      <Component {...pageProps} />
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Solway - Plan your itineries</title>
+            </Head>
+            <WalletConnectionProvider>
+                <Component {...pageProps} />
+            </WalletConnectionProvider>
+        </>
+    )
 }
 
-export default MyApp;
-
-const WalletApp = (props) => {
-  const { Component, pageProps } = props;
-  const network = WalletAdapterNetwork.Devnet;
-
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  const wallets = useMemo(
-      () => [
-
-          new UnsafeBurnerWalletAdapter(),
-      ],
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [network]
-  );
-
-  return (
-      <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect>
-              <WalletModalProvider>
-                  <Component {...pageProps} />
-              </WalletModalProvider>
-          </WalletProvider>
-      </ConnectionProvider>
-  );
-};
+export default MyApp
